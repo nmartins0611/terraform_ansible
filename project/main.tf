728e54b1549cfc5720cabc5c2d36b9184cdbe37d
nmartins0611 <62961138+nmartins0611@users.noreply.github.com>
2023-05-15 12:10:39 +0200

    Update main.tf

--- project/main.tf
+++ project/main.tf
@@ -1,7 +1,11 @@
-# Use AWS provider for Terraform
-
-provider "aws" {
-  region = "eu-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
 }
 
 resource "aws_instance" "basic_rhel" {
